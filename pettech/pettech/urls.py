65e83917cca1791dbbf6e6bc6cb58f995635f1ba
“pckzy” <sarun.debsirin@gmail.com>
2025-09-26 04:23:59 +0700

    add media when debug=true

--- pettech/pettech/urls.py
+++ pettech/pettech/urls.py
@@ -23,7 +23,7 @@ from petsitter_finder.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("petsitter_finder.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
